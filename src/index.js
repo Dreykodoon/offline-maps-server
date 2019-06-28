bac53345de8a78e81d8915264030b9f52d88e35e
Mihai Cioban <ciobanmihai@gmail.com>
2019-06-28 15:06:21 +0300

    Added a koa server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('hello');
+const Koa = require('koa');
+const Router = require('koa-router');
+
+
+const app = new Koa();
+const router = new Router();
+
+router
+  .get('/', (ctx) => {
+    ctx.body = 'Hello, World!';
+  });
+
+app
+  .use(router.routes());
+
+const port = 8080;
+
+console.log('Listening on port ', port);
+app.listen(port);
